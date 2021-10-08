02da13e93236cd3f607668f7482d443f953dcc65
alicjadabr <takiem@gmail.com>
2021-10-08 13:02:38 +0200

    com

---
+++ src/Zad1.java
@@ -0,0 +1,19 @@
+public class Zad1 {
+    public static  void main(String[] args){
+        //Zad.1
+        //System.out.println(31 + 29 + 31);
+        //Zad.2
+        //int sum = 0;
+        //for(int i=1; i<11; i++) {
+        //    sum += i;
+        //}
+        //System.out.println(sum);
+        //Zad.3
+        int sum2 = 0;
+        for(int i=1; i<11; i++) {
+            sum2 += i;
+        }
+        System.out.println(sum2);
+
+    }
+}
