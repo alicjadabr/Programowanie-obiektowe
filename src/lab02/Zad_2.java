67f9669fb56471882979f927afddbd4670d463b3
alicjadabr <alicja.dab27@gmail.com>
2021-10-19 16:21:38 +0200

    zad.2

--- src/lab02/Zad_2.java
+++ src/lab02/Zad_2.java
@@ -7,7 +7,7 @@ public class Zad_2 {
         System.out.println("Podaj liczbe naturalna: ");
         int n = in.nextInt();
         // Zad.2.2
-        /*
+
         float sum2 = 0;
         for (int i = 1; i <= n; i++) {
             System.out.println("Podaj a" + i + ":");
@@ -17,15 +17,15 @@ public class Zad_2 {
             }
         }
         System.out.println("Podwojona suma dodatnich wyrazow ciagu wynosi: " + (2*sum2));
-        */
+
         // Zad.2.3
-        /*
+
         int dodatnie = 0;
         int ujemne = 0;
         int zera = 0;
         for (int i = 1; i <= n; i++) {
             System.out.println("Podaj a" + i + ":");
-            float a = IN.nextFloat();
+            float a = in.nextFloat();
             if (a > 0) {
                 dodatnie += 1;
             } else if (a == 0) {
@@ -35,9 +35,9 @@ public class Zad_2 {
             }
         }
         System.out.println("Wsrod podanych liczb jest: " + zera + " zer, " + dodatnie + " liczb dodatnich i " + ujemne + " liczb ujemnych.");
-        */
+
         // Zad.2.4
-        /*
+
         float[] tab2 = new float[n];
         for(int i=0; i<n; i++){
             System.out.println("Podaj a" + (i+1) + ":");
@@ -55,23 +55,22 @@ public class Zad_2 {
             }
         }
         System.out.println("Najwieksza z podanych liczba to: " + tab2[max] + " , a najmniejsza to: " + tab2[min]);
-        */
+
         // Zad.2.5
-        /*
-        float tab2[] = new float[n];
+
+        float tab3[] = new float[n];
         for(int i=0; i<n; i++) {
             System.out.println("Podaj a" + (i + 1) + ":");
             float x = in.nextFloat();
-            tab2[i] = x;
+            tab3[i] = x;
         }
 
         int pary = 0;
         for(int i=0; i<n-1; i++){
-            if(tab2[i]>0 && tab2[i+1]>0){
+            if(tab3[i]>0 && tab3[i+1]>0){
                 pary += 1;
             }
         }
         System.out.println("Mamy " + pary + " takich par liczb.");
-        */
     }
 }
